6d76b3e51b6046185d424342bb13f8b24b6a9ee9
rqp <noobball111+github@gmail.com>
2026-01-31 10:19:23 +0700

    added uuid

--- Utils/Signal.py
+++ Utils/Signal.py
@@ -1,24 +1,36 @@
+import uuid
+
 class Connection:
-    def __init__(self, event, func):
-        self.event = event
-        self.func = func
+    def __init__(self, event, connection_id):
+        self._event = event
+        self._id = connection_id
         self.connected = True
 
     def Disconnect(self):
-        if self.func in self.event.Listeners:
-            del self.event.Listeners[self.func]
+        if self.connected and self._event:
+            # Remove this specific connection from the dictionary
+            if self._id in self._event.Listeners:
+                del self._event.Listeners[self._id]
+            
+            # Wipe references for Garbage Collection
+            self._event = None
+            self._id = None
             self.connected = False
-        
-        self = None
+            self = None
 
 class Event:
     def __init__(self):
         self.Listeners = {}
 
     def Connect(self, func):
-        self.Listeners[func] = func
-        return Connection(self, func)
+        # Generate a unique ID for this specific connection
+        connection_id = uuid.uuid4()
+        self.Listeners[connection_id] = func
+        
+        # Pass the ID to the connection object
+        return Connection(self, connection_id)
 
     def Fire(self, *args):
+        # Iterate over a copy of values to allow Disconnect during Fire
         for func in list(self.Listeners.values()):
             func(*args)
